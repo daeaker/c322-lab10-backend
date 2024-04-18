8372e2aed3b2ef1a3f445a19ea73da4f7926d9bf
daeaker <daeaker@iu.edu>
2024-04-18 12:55:48 -0400

    lmao

--- src/main/java/edu/iu/habahram/ducksservice/repository/CustomerFileRepository.java
+++ src/main/java/edu/iu/habahram/ducksservice/repository/CustomerFileRepository.java
@@ -14,7 +14,9 @@ import java.util.ArrayList;
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class CustomerFileRepository {
     private static final Logger LOG =
             LoggerFactory.getLogger(CustomerFileRepository.class);
